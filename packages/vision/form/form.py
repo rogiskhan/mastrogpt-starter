bf9e3df3093d6badb52915b48b7e9a5660c566ce
rogiskhan <monaldi.igor@gmail.com>
2025-07-06 12:51:37 +0000

    exercise 5 done!

--- packages/vision/form/form.py
+++ packages/vision/form/form.py
@@ -1,6 +1,8 @@
 import os, requests as req
 import vision2 as vision
+from store.bucket import Bucket
 
+import uuid
 USAGE = "Please upload a picture and I will tell you what I see"
 FORM = [
   {
@@ -10,7 +12,7 @@ FORM = [
     "type": "file"
   },
 ]
-
+s3 = Bucket()
 def form(args):
   res = {}
   out = USAGE
@@ -19,8 +21,14 @@ def form(args):
   if type(inp) is dict and "form" in inp:
     img = inp.get("form", {}).get("pic", "")
     print(f"uploaded size {len(img)}")
+    #storing to s3
+    myuuid = uuid.uuid4()
+    import datetime;
+    ct = datetime.datetime.now()
+    name = str(myuuid) + "_" + str(ct)
     vis = vision.Vision(args)
     out = vis.decode(img)
+    s3.write(name, out)
     res['html'] = f'<img src="data:image/png;base64,{img}">'
     
   res['form'] = FORM
